51752cae3018ce46b137992e528118d7c826fd53
samah <ibrahim.magdy24@gmail.com>
2019-08-15 14:34:44 +0200

    added try.java file

---
+++ try.java
@@ -0,0 +1,28 @@
+
+public class Main{ //begin of Main Class
+
+  public static int maxmum (int a,int b,int c, int d){
+    int max;
+
+
+   // your code here
+
+
+
+    return max;
+  }
+
+  public static void main (String args[]){ // begin of main Method
+
+
+  //call the method here
+  
+    maxmum(50,40,80,100);
+
+
+
+
+
+ } // the end of method main
+
+} // the end of class Main
